dd8e692e7b0468f268c7934e8e74bade6d7ea0e7
nzilla <victorbliss044@gmail.com>
2024-08-10 15:54:59 +0100

    commit

---
+++ main.js
@@ -0,0 +1,14 @@
+//get element varible
+const nav_btn = document.getElementById('btn');
+const bar1 = document.getElementById('line1');
+ 
+
+nav_btn.addEventListener('click', function(){
+
+
+  console.log(nav_btn)
+  console.log(bar1)
+}
+)
+
+
