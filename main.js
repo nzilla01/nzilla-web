b231c8ed2c95f48f6e7725ebdea65b9cb824c7b2
nzilla <victorbliss044@gmail.com>
2024-08-20 12:38:48 +0100

    commit

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //get element varible
+
 const nav_btn = document.getElementById('btn');
 const bar1 = document.getElementById('line1');
 const bar2 = document.getElementById('line2');
@@ -6,10 +7,9 @@ const bar3 = document.getElementById('line3');
  
 
 nav_btn.addEventListener('click', function(){
-  bar1.classList.toggle('active')
-  bar2.classList.toggle('active')
-  bar3.classList.toggle('active')
+  bar1.classList.toggle('active');
+  bar2.classList.toggle('close');
+  bar3.classList.toggle('open');
 }
-)
-
+);
 
